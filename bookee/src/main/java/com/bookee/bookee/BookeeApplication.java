790a2234d7ae293d0d049fcf413777676b9c79b2
sieunnnnx <sieunnnnx@gmail.com>
2026-04-12 21:11:48 +0900

    chore: 프로젝트 생성 (#1)

---
+++ bookee/src/main/java/com/bookee/bookee/BookeeApplication.java
@@ -0,0 +1,13 @@
+package com.bookee.bookee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookeeApplication.class, args);
+    }
+
+}
