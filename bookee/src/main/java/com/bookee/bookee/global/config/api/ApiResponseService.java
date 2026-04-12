c3a6fefe2abc461df779c746129433c986b4e279
sieunnnnx <sieunnnnx@gmail.com>
2026-04-12 22:30:46 +0900

    chore: 공통 api 응답구조 내 httpStatus 추가 (#6)

--- bookee/src/main/java/com/bookee/bookee/global/config/api/ApiResponseService.java
+++ bookee/src/main/java/com/bookee/bookee/global/config/api/ApiResponseService.java
@@ -1,29 +1,26 @@
 package com.bookee.bookee.global.config.api;
 
-import com.bookee.bookee.global.config.exception.GlobalErrorCode;
+import com.bookee.bookee.global.config.exception.ExceptionCode;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApiResponseService {
-    private final String SUCCESS_CODE = "00";
-    private final String SUCCESS_MESSAGE = "Request was successful.";
-
 
     public <T> ApiResponse<T> success(T data) {
         return ApiResponse.<T>builder()
                 .isSuccess(true)
-                .code(SUCCESS_CODE)
+                .code(SuccessCode.SUCCESS.getCode())
+                .message(SuccessCode.SUCCESS.getMessage())
                 .data(data)
-                .message(SUCCESS_MESSAGE)
                 .build();
     }
 
-    public ApiResponse<?> fail(GlobalErrorCode errorCode) {
+    public ApiResponse<?> fail(ExceptionCode errorCode) {
         return ApiResponse.builder()
                 .isSuccess(false)
                 .code(errorCode.getCode())
-                .data(null)
                 .message(errorCode.getMessage())
+                .data(null)
                 .build();
     }
 }
