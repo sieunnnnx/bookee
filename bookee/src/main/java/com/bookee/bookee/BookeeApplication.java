edefc5ae657cd8d97ce7ba991cf1dabfa54baf74
sieunnnnx <sieunnnnx@gmail.com>
2026-04-15 22:00:08 +0900

    Chore: EnableJpaAuditing 추가 (#8)

--- bookee/src/main/java/com/bookee/bookee/BookeeApplication.java
+++ bookee/src/main/java/com/bookee/bookee/BookeeApplication.java
@@ -2,7 +2,9 @@ package com.bookee.bookee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BookeeApplication {
 
