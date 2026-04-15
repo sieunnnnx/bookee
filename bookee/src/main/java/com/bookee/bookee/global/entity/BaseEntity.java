21b3e0783bd0e3a52e4f65c738de395eebb7aef5
sieunnnnx <sieunnnnx@gmail.com>
2026-04-15 21:59:27 +0900

    chore: BaseEntity 생성 (#8)

---
+++ bookee/src/main/java/com/bookee/bookee/global/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.bookee.bookee.global.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private Long createdBy;
+
+    private Long updatedBy;
+
+    private Boolean isDeleted;
+}
